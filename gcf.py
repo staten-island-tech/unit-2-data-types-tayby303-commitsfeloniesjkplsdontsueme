783e9e0aeb144da5f59244d462e20d306207315b
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-02-05 14:56:56 +0000

    commit

--- gcf.py
+++ gcf.py
@@ -1,5 +1,6 @@
 num1=int(input("Number 1: "))
 num2=int(input("Number 2: "))
+<<<<<<< HEAD
 def gcf(a,b):
   smaller=min(a,b)
   for i in (smaller,0,-1):
@@ -7,3 +8,13 @@ def gcf(a,b):
      return i
     return 1
 print(gcf(num1,num2))
+=======
+def gcf(a,b)
+  smaller=min(a,b)
+    for i(smaller,0,-1)
+      if a%i==0 and b%i==0
+        return i
+      return 1
+ print(gcf(num1,num2))
+>>>>>>> 52c55ee (commit)
+commit
