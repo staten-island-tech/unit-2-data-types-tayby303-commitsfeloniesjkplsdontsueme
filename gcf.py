8d9e7b7af1e260b55b49f1f43c323858db5cd774
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-02-05 14:46:03 -0500

    gcf.py

--- gcf.py
+++ gcf.py
@@ -2,8 +2,8 @@ num1=int(input("Number 1: "))
 num2=int(input("Number 2: "))
 def gcf(a,b):
   smaller=min(a,b)
-  for i in (smaller,0,-1):
+  for i in range(smaller,0,-1):
     if a%i==0 and b%i==0:
      return i
-    return gcf
+  return gcf
 print(gcf(num1,num2))
