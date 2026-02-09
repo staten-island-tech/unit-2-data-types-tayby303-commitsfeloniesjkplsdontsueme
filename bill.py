3767632248fe8a1771576f167d3addd0317d665d
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-02-09 10:11:55 -0500

    commit

--- bill.py
+++ bill.py
@@ -1,2 +1,10 @@
-service=input("good, okay, or bad? ")
-bill=int(input("How much do you want to tip? "))
+service=input("Was the service great, good, okay, or bad? ")
+if service=="great":
+    bill=("25%")
+elif service=="good":
+    bill=("20%")
+elif service=="okay":
+    bill=("15%")
+elif service=="bad":
+    bill=("0%")
+print("The tip will be "+bill)
