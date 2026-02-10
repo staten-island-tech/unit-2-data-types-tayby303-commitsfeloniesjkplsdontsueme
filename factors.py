f8a864d881b5cea7de33dad235414782fa67ba39
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-02-10 09:51:47 -0500

    commit

--- factors.py
+++ factors.py
@@ -1,7 +1,7 @@
 n=int(input("What number? "))
-def factor(x)
+def factor(x):
   factors=[]
-  for i in range(1,n+1)
+  for i in range(1,n+1):
     if n % i == 0:
       factors.append(i)
   return factors
